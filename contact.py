d8a4938c5472ca3df69c39a5d803d02a56dbdc95
lj5619 <leon.olaprath@gmail.com>
2025-03-17 10:59:32 +0530

    Leon - [add] - added create contact functionality

---
+++ contact.py
@@ -0,0 +1,15 @@
+class Contact:
+    def __init__(self,first_name, last_name, address, city, state, zip_code, phone_num, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.phone_num = phone_num
+        self.email = email
+
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name}\n"
+                f"Address: {self.address}, {self.city}, {self.state} {self.zip_code}\n"
+                f"Phone: {self.phone_num}\nEmail: {self.email}")
